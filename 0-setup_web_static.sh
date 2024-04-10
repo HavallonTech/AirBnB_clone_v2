398d58ed9f81c9d37d48f9d3896305847d1c95b0
HavallonTech <kestercode@gmail.com>
2024-04-10 07:32:56 +0100

    ALX

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,28 +12,28 @@ else
     sudo apt-get  install -y nginx
 fi
 # make the required directory
-sudo mkdir -p data/web_static/releases/test
-sudo mkdir -p data/web_static/shared
 
-#creating a fake html file
-echo "
-<html>
- <head>
- </head>
- <body>
-     Holberton School
- </body>
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-# creating a symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
 #changing of ownership
 sudo chown -R ubuntu:ubuntu /data/
 
 #updating nginx configurations
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
-
 # Restarting Nginx
 sudo service nginx restart
 exit 0
