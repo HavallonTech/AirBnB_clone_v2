f03f5fafc8d39db76c9f5e047177e6ae9dc1b4f2
HavallonTech <kestercode@gmail.com>
2024-04-10 04:56:33 +0100

    Airbnb

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,35 @@
-#!/usr/bin/env python3
-
-from fabric.api import *
-from datetime import datetime
-
-
-def do_pack():
-    the_time = datetime.now()
-    time_string = the_time.strftime("%Y%m%d%H%M%S")
-    print(time_string)
-    local(f"sudo tar -cvzf versions/web_static_{time_string}.tgz web_static")
+#!/usr/bin/env bash
+"""
+a Bash script that sets up your web servers for the deployment of web_static
+"""
 
 
-do_pack()
+# check if nginx is installed
+if command -v nginx &> /dev/null; then
+    echo "Nginx is already installed."
+else
+    echo "Nginx is not installed."
+    echo "Installing process..."
+    sudo apt-get -y update
+    sudo apt-get  install -y nginx
+fi
+# make the required directory
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+#creating a fake html file
+echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
+
+# creating a symbolic link
+ln -s /data/web_static/releases/test/ /data/web_static/current
+
+#changing of ownership
+sudo chown -R ubuntu:ubuntu /data/
+
+#updating nginx configurations
+sudo sed -i 's|^.*server_name.*$|&\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n|' /etc/nginx/sites-available/default
+
+# Restarting Nginx
+sudo service nginx restart
+
+exit 0
