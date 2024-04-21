0e1ffe0dd174551d3d286850cbf360117799c053
HavallonTech <kestercode@gmail.com>
2024-04-21 07:36:01 -0700

    Flask task

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,19 +4,22 @@ A script to start flask and the application must be
 listening on port 5000 host 0.0.0.0
 """
 
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
 
 #define the route for the task roor URL'/'
-@app.route('/', start_slashes=False)
-deg hello_hbnb():
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
     """
     Display 'Hello BNB'
     """
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-    #Start Flask Dev Server
-    #lsiten on the network infrastructure required
+    """
+    Start Flask Dev Server
+    lsiten on the network infrastructure required
+    """
+
     app.run(host='0.0.0.0', port=5000)
