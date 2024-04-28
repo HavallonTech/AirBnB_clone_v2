d5e7025d950cf5268414cff42cdfaa264ca1174e
HavallonTech <kestercode@gmail.com>
2024-04-28 14:57:16 -0700

    Flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,75 +1,46 @@
 #!/usr/bin/python3
 """
-A script to start flask and the application must be
-listening on port 5000 host 0.0.0.0
+ starts a Flask web application:
 """
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-"""define the route for the task roor URL'/'
-"""
-
-
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """
-    Display 'Hello BNB'
-    """
-    return "Hello HBNB!"
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_and_text(text):
-    """
-    Display 'C followed by the text passed as variable'
-    Replace underscore with the text variable
-    """
-    text_formatted = text.replace('_', ' ')
-    return f"C {text_formatted}"
+def passed_text(text):
+    if "_" in text:
+        text = text.replace("_", " ")
+    return f'C {text}'
 
 
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-def python_and_text(text):
-    """
-    Display 'C followed by the text passed as variable'
-    Replace underscore with the text variable
-    """
-    text_formatted = text.replace('_', ' ')
-    return f"Python {text_formatted}"
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def is_number(n):
-    """
-    Display 'the value of N only if N is a number
-    """
-    return f"{n} is a number"
+def python(text='is cool'):
+    if "_" in text:
+        text = text.replace("_", " ")
+    return f'Python {text}'
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """
-    Display a HTML document
-    """
+    return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
     return render_template('5-number.html', number=n)
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """
-    Display 'HBNB'
-    """
-    return "HBNB"
-
-
-if __name__ == "__main__":
-    """
-    Start Flask Dev Server
-    lsiten on the network infrastructure required
-    """
-
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
